62b3ff3d64e071bf5704e820bc720a57249425ae
Artemov Ivan (ZOXEXIVO) <zoxexivo@gmail.com>
2024-12-22 18:31:54 +0300

    fix mf logic

--- src/core/src/match/engine/player/strategies/midfielders/states/walking/mod.rs
+++ src/core/src/match/engine/player/strategies/midfielders/states/walking/mod.rs
@@ -2,8 +2,8 @@ use crate::common::loader::DefaultNeuralNetworkLoader;
 use crate::common::NeuralNetwork;
 use crate::r#match::midfielders::states::MidfielderState;
 use crate::r#match::{
-    ConditionContext, StateChangeResult, StateProcessingContext,
-    StateProcessingHandler, SteeringBehavior,
+    ConditionContext, StateChangeResult, StateProcessingContext, StateProcessingHandler,
+    SteeringBehavior,
 };
 use crate::IntegerUtils;
 use nalgebra::Vector3;
@@ -18,46 +18,51 @@ pub struct MidfielderWalkingState {}
 impl StateProcessingHandler for MidfielderWalkingState {
     fn try_fast(&self, ctx: &StateProcessingContext) -> Option<StateChangeResult> {
         if ctx.team().is_control_ball() {
+            if ctx.ball().is_towards_player_with_angle(0.8) {
+                return Some(StateChangeResult::with_midfielder_state(
+                    MidfielderState::Intercepting,
+                ));
+            }
+            
             return Some(StateChangeResult::with_midfielder_state(
                 MidfielderState::Running,
             ));
-        }
-
-        // 1. If the defender is on their own side and the ball is close, transition to Intercepting
-        if ctx.ball().is_towards_player_with_angle(0.8) && ctx.ball().distance() < 250.0 {
-            return Some(StateChangeResult::with_midfielder_state(
-                MidfielderState::Pressing,
-            ));
-        }
-
-        let nearby_opponents = ctx.players().opponents().nearby(150.0).collect::<Vec<_>>();
-        if !nearby_opponents.is_empty() {
-            // If there are nearby opponents, assess the situation
-            let ball_distance = ctx.ball().distance();
-
-            let mut closest_opponent_distance = f32::MAX;
-            for opponent in &nearby_opponents {
-                let distance = ctx.player().distance_to_player(opponent.id);
-                if distance < closest_opponent_distance {
-                    closest_opponent_distance = distance;
-                }
-            }
-
-            if ball_distance < 100.0 && closest_opponent_distance < 50.0 {
-                // If the ball is close and an opponent is very close, transition to Tackling state
-                return Some(StateChangeResult::with_midfielder_state(
-                    MidfielderState::Tackling,
-                ));
-            } else if ball_distance < 200.0 {
-                // If the ball is moderately close, transition to Pressing state
+        } else {
+            if ctx.ball().is_towards_player_with_angle(0.8) && ctx.ball().distance() < 250.0 {
                 return Some(StateChangeResult::with_midfielder_state(
                     MidfielderState::Pressing,
                 ));
-            } else {
-                // If the ball is far, transition to Running state to get closer to the action
-                return Some(StateChangeResult::with_midfielder_state(
-                    MidfielderState::Running,
-                ));
+            }
+
+            let nearby_opponents = ctx.players().opponents().nearby(150.0).collect::<Vec<_>>();
+            if !nearby_opponents.is_empty() {
+                // If there are nearby opponents, assess the situation
+                let ball_distance = ctx.ball().distance();
+
+                let mut closest_opponent_distance = f32::MAX;
+                for opponent in &nearby_opponents {
+                    let distance = ctx.player().distance_to_player(opponent.id);
+                    if distance < closest_opponent_distance {
+                        closest_opponent_distance = distance;
+                    }
+                }
+
+                if ball_distance < 100.0 && closest_opponent_distance < 50.0 {
+                    // If the ball is close and an opponent is very close, transition to Tackling state
+                    return Some(StateChangeResult::with_midfielder_state(
+                        MidfielderState::Tackling,
+                    ));
+                } else if ball_distance < 200.0 {
+                    // If the ball is moderately close, transition to Pressing state
+                    return Some(StateChangeResult::with_midfielder_state(
+                        MidfielderState::Pressing,
+                    ));
+                } else {
+                    // If the ball is far, transition to Running state to get closer to the action
+                    return Some(StateChangeResult::with_midfielder_state(
+                        MidfielderState::Running,
+                    ));
+                }
             }
         }
 
