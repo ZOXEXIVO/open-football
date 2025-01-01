38a8b6f2de97b392dc3cb08a3b7320080b9ca344
Artemov Ivan (ZOXEXIVO) <zoxexivo@gmail.com>
2025-01-01 19:09:22 +0300

    fix

--- src/core/src/match/engine/player/strategies/goalkeepers/states/passing/mod.rs
+++ src/core/src/match/engine/player/strategies/goalkeepers/states/passing/mod.rs
@@ -3,9 +3,7 @@ use crate::common::NeuralNetwork;
 use crate::r#match::events::Event;
 use crate::r#match::goalkeepers::states::state::GoalkeeperState;
 use crate::r#match::player::events::{PassingEventContext, PlayerEvent};
-use crate::r#match::{
-    ConditionContext, StateChangeResult, StateProcessingContext, StateProcessingHandler,
-};
+use crate::r#match::{ConditionContext, MatchPlayerLite, StateChangeResult, StateProcessingContext, StateProcessingHandler};
 use nalgebra::Vector3;
 use std::sync::LazyLock;
 
@@ -23,19 +21,16 @@ impl StateProcessingHandler for GoalkeeperPassingState {
             ));
         }
 
-        let players = ctx.players();
-        let teammates = players.teammates();
-
-        let mut nearest_teammates = teammates.nearby(200.0);
-
-        if let Some(teammate) = nearest_teammates.next() {
+        if let Some(teammate) = self.find_best_pass_option(ctx) {
             return Some(StateChangeResult::with_goalkeeper_state_and_event(
                 GoalkeeperState::Standing,
-                Event::PlayerEvent(PlayerEvent::PassTo(PassingEventContext::build()
-                    .with_player_id(ctx.player.id)
-                    .with_target(teammate.position)
-                    .with_force(ctx.player().pass_teammate_power(teammate.id))
-                    .build())),
+                Event::PlayerEvent(PlayerEvent::PassTo(
+                    PassingEventContext::build()
+                        .with_player_id(ctx.player.id)
+                        .with_target(teammate.position)
+                        .with_force(ctx.player().pass_teammate_power(teammate.id))
+                        .build()
+                )),
             ));
         }
 
@@ -51,4 +46,19 @@ impl StateProcessingHandler for GoalkeeperPassingState {
     }
 
     fn process_conditions(&self, _ctx: ConditionContext) {}
+}
+
+impl GoalkeeperPassingState {
+    fn find_best_pass_option(&self, ctx: &StateProcessingContext) -> Option<MatchPlayerLite> {
+        let players = ctx.players();
+        let teammates = players.teammates();
+
+        teammates
+            .nearby(200.0)
+            .max_by(|a, b| {
+                let opponents_near_a = players.opponents().nearby_raw(20.0).count();
+                let opponents_near_b = players.opponents().nearby_raw(20.0).count();
+                opponents_near_a.cmp(&opponents_near_b).reverse()
+            })
+    }
 }
