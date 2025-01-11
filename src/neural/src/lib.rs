70a9ffb6b37bf915c4d3eca4454f0f1cadae3d6a
Artemov Ivan (ZOXEXIVO) <zoxexivo@gmail.com>
2025-01-11 18:22:48 +0300

    move neural to separate package

---
+++ src/neural/src/lib.rs
@@ -0,0 +1,13 @@
+mod r#match;
+
+use burn::backend::NdArray;
+use burn::backend::ndarray::NdArrayDevice;
+
+pub use burn::prelude::*;
+pub use r#match::*;
+
+// DEFAULTS
+
+pub type DefaultNeuralBackend = NdArray;
+
+pub const DEFAULT_NEURAL_DEVICE: NdArrayDevice = NdArrayDevice::Cpu;
